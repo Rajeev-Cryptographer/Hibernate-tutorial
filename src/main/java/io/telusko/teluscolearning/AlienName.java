a776f718e317779d56f8adfaf16470e04be2a935
Rajeev-Cryptographer <183406481+Rajeev-Cryptographer@users.noreply.github.com>
2025-12-30 23:34:12 +0530

    Add getters and setters for AlienName fields

--- src/main/java/io/telusko/teluscolearning/AlienName.java
+++ src/main/java/io/telusko/teluscolearning/AlienName.java
@@ -1,5 +1,42 @@
 package io.telusko.teluscolearning;
 
 public class AlienName {
+          private String fname;
+          private String lname;
+          private String mname;
+          // Getter and Setter for fname
+    public String getFname() {
+        return fname;
+    }
 
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    // Getter and Setter for lname
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    // Getter and Setter for mname
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    public String toString() {
+        return "Person{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", mname='" + mname + '\'' +
+                '}';
+    }
+  
+  
+  
 }
